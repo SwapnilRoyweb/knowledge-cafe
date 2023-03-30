bbed8b4655620ec2732704c109e0ecf4d2535573
swapnilRoyWeb <swapnilroyweb@gmail.com>
2023-03-31 03:02:26 +0600

    bookmark blog or bookmark history added

--- src/History/History.jsx
+++ src/History/History.jsx
@@ -4,8 +4,14 @@ import './History.css';
 const History = () => {
     return (
         <div>
-            <div className='read-time'>
+            <div className='read-time rounded'>
                 <p className='fw-bold'>Spend time on read: 150min</p>
+            </div>
+            <div className='mt-3 grey p-3 rounded'>
+                <h5 className='fw-bold text-center'>Bookmarked Blogs: 4</h5>
+                <div className='bg-light rounded'>
+                    
+                </div>
             </div>
         </div>
     );
